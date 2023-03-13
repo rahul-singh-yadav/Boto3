9658bb5bdfdf149658d0d110498b565acb5b7ecc
Rahul Yadav <rahul.yadav@prefme.com>
2023-03-13 13:11:48 +0530

    fixed InvalidationBatchAlreadyExist exception using datetime.now()

--- CLOUDFRONT/main.py
+++ CLOUDFRONT/main.py
@@ -1,13 +1,13 @@
 import boto3
 import os
 import dotenv
-from datetime import date
+from datetime import datetime
 
 # Load environment variables from .env
 dotenv.load_dotenv()
 
 # Fetch current datetime.
-current = date.today()
+current = datetime.now()
 
 # Use client()
 client = boto3.client('cloudfront')
@@ -18,18 +18,26 @@ id = os.environ.get('CLOUDFRONT_ID')
 # Fetch invalidation path from environemnt variables.
 path = os.environ.get('INVALIDATION_PATH')
 
-
 # Save responses
-response = client.create_invalidation(
+try:
+    response = client.create_invalidation(
     DistributionId = id,
-    InvalidationBatch = {
-        'Paths': {
-            'Quantity': 1, 
-            'Items': [str(path)]
-        },
-        'CallerReference': str(current)
-    }
-)
+        InvalidationBatch = {
+            'Paths': {
+                'Quantity': 1, 
+                'Items': [str(path)]
+            },
+            'CallerReference': str(current)
+        }
+    )
 
-# Output response from cloudfront
-print(response)
+    # Output response from cloudfront
+    print(response)
+
+except client.exceptions.InvalidationBatchAlreadyExists as e:
+    # Throws exception error message at stdout
+    print(f"Invalidation requested already exist, try a unique caller reference: {e}")
+
+# except exception as e1:
+#     # Throws any other exceptions occured at runtime.
+#     print(f"An error occured: {e1}")
