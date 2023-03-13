45dca238cf0d27dd0858ac35f1cd98b27160ed53
Rahul Yadav <rahul.yadav@prefme.com>
2023-03-13 12:12:43 +0530

    modified code to use environment variables

--- CLOUDFRONT/main.py
+++ CLOUDFRONT/main.py
@@ -1,20 +1,35 @@
 import boto3
+import os
+import dotenv
 from datetime import date
 
-# Fetch current datetime
+# Load environment variables from .env
+dotenv.load_dotenv()
+
+# Fetch current datetime.
 current = date.today()
 
 # Use client()
 client = boto3.client('cloudfront')
 
+# Fetch DistributionId from environment variables.
+id = os.environ.get('CLOUDFRONT_ID')
+
+# Fetch invalidation path from environemnt variables.
+path = os.environ.get('INVALIDATION_PATH')
+
+
 # Save responses
 response = client.create_invalidation(
-    DistributionId='<ID>',
-    InvalidationBatch={
+    DistributionId = id,
+    InvalidationBatch = {
         'Paths': {
             'Quantity': 1, 
-            'Items': ['/createpataaminified/smart-city-create-pataa.js']
+            'Items': [str(path)]
         },
         'CallerReference': str(current)
     }
 )
+
+# Output response from cloudfront
+print(response)
