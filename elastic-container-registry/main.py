6f44bdcfef5c7fbf2385d24cb4db2745c2e2afde
Rahul Yadav <rahul.yadav@prefme.com>
2023-03-21 19:08:17 +0530

    Added initial code for fetching ecr repo data and saving it a text file locally

--- elastic-container-registry/main.py
+++ elastic-container-registry/main.py
@@ -1,18 +1,55 @@
 import os
 import json
 import boto3
+import time
 import botocore.exceptions
 
-# Create a client to interact with service 'ecr'
+# Sets filepath
+filepath = os.path.join(os.getcwd(), 'repositories.txt')
+
+# Creates a client to interact with service 'ecr'
 client = boto3.client('ecr')
 
 # repositories : type(dict)
-repositories = client.describe_repositories()['repositories']
+# key: 'repositories'; using dict. indexing
+repositories_list = client.describe_repositories()['repositories']
 
-# Loop through all repos and get repository names
-for repository in repositories:
-    repository_names = repository.get('repositoryName')
-    registry_id = repository.get('registryId')
+# Open file for writing fetched data.
+with open(filepath, "a") as file:
 
-    # Return as a tuple of values
-    print((repository_names, registry_id))
+    # Loop through all repos and get repository names
+    for repository in repositories_list:
+
+        repository_name = repository.get('repositoryName')
+        repository_uri = repository.get('repositoryUri')
+        registry_id = repository.get('registryId')
+
+        # Write to a file, if the file doesn't exist... create it.
+        file.write(str(f"{repository_name}, {repository_uri}") + str('\n'))
+
+# Post writing to file, close file.    
+file.close()
+
+print(f"Data written at {os.path.join(os.getcwd(), 'requirements.txt')} \n")
+
+# Read lifecycle policy
+print(f"Opening lifecycle policy rules file at {os.path.join(os.getcwd(), 'requirements.txt')} \n")
+policy_data = open('./lifecycle_policy.json', "r")
+
+rules = policy_data.read()
+
+print(f"Current set of rules are: \n {rules}")
+
+for rules in repositories_list:
+    client.put_lifecycle_policy(
+
+        registryId = str(rules.get('registryId')),
+        repositoryName = str(rules.get('repositoryName')),
+        lifecyclePolicyText = str(rules)
+    )
+
+time.sleep(3)
+
+print(f"Closing file now...  \n")
+
+policy_data.close()
