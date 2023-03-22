07ef0359df344157693745214304b15ec984c0d1
Rahul Yadav <rahul.yadav@prefme.com>
2023-03-22 12:38:06 +0530

    improved code with successfull PUT jobs made

--- elastic-container-registry/main.py
+++ elastic-container-registry/main.py
@@ -1,55 +1,77 @@
 import os
 import json
 import boto3
-import time
 import botocore.exceptions
+from datetime import datetime
 
-# Sets filepath
+""" 1. Sets filepath for a text file that will be created.
+    2. Creates a boto3 client to interact with ecr api.
+    3. Fetches a dictionary containing all repositories at aws account in a region.
+       # key: 'repositories'; using dict. indexing
+    4. Logs message for file ready to be opened and written to at stdout.
+
+    Note: The aws account and region are set using aws config profiles.
+"""
+
 filepath = os.path.join(os.getcwd(), 'repositories.txt')
 
-# Creates a client to interact with service 'ecr'
 client = boto3.client('ecr')
 
-# repositories : type(dict)
-# key: 'repositories'; using dict. indexing
-repositories_list = client.describe_repositories()['repositories']
+repositories_dict = client.describe_repositories()['repositories']
 
-# Open file for writing fetched data.
+print(f"Opening file for writing fetched data.\n")
+
 with open(filepath, "a") as file:
+    """ 1. File is opened using context manager as 'file'.
+        2. Writes current date-time to file for each successfull PUT job.
+        3. Goes through all repositories in `repository_dict` object & sets them as:
+            - Respository names.
+            - Repository URIs.
+            - Registry Id's. 
+        4. Closes the file once data is written in the file named `requirements.txt` at cwd.
 
-    # Loop through all repos and get repository names
-    for repository in repositories_list:
+        Note: If the files doesn't exist, it creates one and writes data to it.
+    """
+
+    file.write(str(f"Last succesfull write: [{datetime.now()}] \n"))
+
+    for repository in repositories_dict:
 
         repository_name = repository.get('repositoryName')
         repository_uri = repository.get('repositoryUri')
         registry_id = repository.get('registryId')
 
-        # Write to a file, if the file doesn't exist... create it.
         file.write(str(f"{repository_name}, {repository_uri}") + str('\n'))
 
-# Post writing to file, close file.    
 file.close()
 
 print(f"Data written at {os.path.join(os.getcwd(), 'requirements.txt')} \n")
 
-# Read lifecycle policy
 print(f"Opening lifecycle policy rules file at {os.path.join(os.getcwd(), 'requirements.txt')} \n")
-policy_data = open('./lifecycle_policy.json', "r")
 
-rules = policy_data.read()
+with open('./lifecycle_policy.json', "r") as policy_data:
+    """ 1. Opens a file containing ecr lifecycle policy as json data.
+        3. Reads the json data to a variable named `rule`.
+        2. Goes through all repositories in `repository_dict` objects.
+        3. Inside the loop, calls put_lifecycle_policy method using `client` object and PUTS the following:
+            - registryId
+            - repositoryName
+            - rule
+        4. Closes the file post each successfull PUT job.
+    """
+    rule = policy_data.read()
 
-print(f"Current set of rules are: \n {rules}")
+    print(f"Current set of rules are: \n \n {rule} \n")
 
-for rules in repositories_list:
-    client.put_lifecycle_policy(
+    for rules in repositories_dict:
 
-        registryId = str(rules.get('registryId')),
-        repositoryName = str(rules.get('repositoryName')),
-        lifecyclePolicyText = str(rules)
-    )
+        response = client.put_lifecycle_policy(
 
-time.sleep(3)
+            registryId = str(rules.get('registryId')),
+            repositoryName = str(rules.get('repositoryName')),
+            lifecyclePolicyText = str(rule)
+        )
 
-print(f"Closing file now...  \n")
+    print(f"Closing file now...")
 
 policy_data.close()
